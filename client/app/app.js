75638b43c7c8b4a34615fa82c4f7959ee8a61819
Tim Arioto <timarioto@gmail.com>
2016-10-25 13:33:35 -0700

    Save for rebase

--- client/app/app.js
+++ client/app/app.js
@@ -1,12 +1,19 @@
-angular.module('greenfield', ['ngRoute'])
+angular.module('greenfield', ['ui.router'])
 
-.config(function ($routeProvider, $httpProvider) {
-  $routeProvider
-    .when('/', {
-      templateUrl: 'app/views/home.html',
-      controller: 'app/controllers/home.js'
-    })
-    .otherwise({
-      redirectTo: '/'
-    });
+.config(function ($stateProvider, $httpProvider) {
+  // $routeProvider
+  //   .when('/', {
+  //     templateUrl: 'app/views/home.html',
+  //     controller: 'app/controllers/home.js'
+  //   })
+  //   .otherwise({
+  //     redirectTo: '/'
+  //   });
+  var homeState = {
+    name: 'home',
+    url: '/',
+    template: '<h3>Its the UI-Router hello world app!</h3>'
+  }
+
+    $stateProvider.state(homeState);
 });
