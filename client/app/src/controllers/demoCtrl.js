06d2641ba1990fd9ef86ed2ef341143afcf21980
anicknam <a.nicknam@gmail.com>
2016-10-25 15:40:25 -0700

    Create custom directive for d3 implementation and rendering

---
+++ client/app/src/controllers/demoCtrl.js
@@ -0,0 +1,35 @@
+(function () {
+  'use strict';
+
+  angular.module('myApp.controllers')
+    .controller('DemoCtrl', function($scope){
+      $scope.title = "GoalTree";
+
+      $scope.d3Data = [
+        {
+          "title": "Graduate from HR!!!",
+          "children": [
+            {
+              "title": "Finish Greenfield Project",
+              "children": [
+                {
+                  "title": "Finish front-end",
+                },
+                {
+                  "title": "Finish back-end",
+                }
+              ]
+            },
+            {
+              "title": "Finish Legacy Project",
+            }
+          ]
+        }
+      ];
+
+      $scope.d3OnClick = function(item){
+        console.log(item.title);
+      };
+    });
+
+}());
