7f0f6e50d1c2f987f55e53aefbd0f3b6f8c44c4d
anicknam <a.nicknam@gmail.com>
2016-10-25 23:51:26 -0700

    Add styling for current node box

--- client/app/src/controllers/d3Ctrl.js
+++ client/app/src/controllers/d3Ctrl.js
@@ -1,46 +1,47 @@
-(function () {
-  'use strict';
 
-  angular.module('myApp.controllers')
-    .controller('d3Ctrl', function($scope){
-      $scope.title = "GoalTree";
+angular.module('myApp.controllers')
+  .controller('d3Ctrl', function($scope){
+    $scope.title = "GoalTree";
 
-      $scope.d3Data = [
-        {
-          "id": 12,
-          "title": "Graduate from HR!!!",
-          "children": [
-            {
-              "id": 34,
-              "title": "Finish Greenfield Project",
-              "children": [
-                {
-                  "id": 56,
-                  "title": "Finish front-end",
-                },
-                {
-                  "id": 78,
-                  "title": "Finish back-end",
-                }
-              ]
-            },
-            {
-              "id": 910,
-              "title": "Finish Legacy Project",
-            }
-          ]
-        }
-      ];
+    $scope.d3Data = [
+      {
+        "id": 12,
+        "title": "Graduate from HR!!!",
+        "children": [
+          {
+            "id": 34,
+            "title": "Finish Greenfield Project",
+            "children": [
+              {
+                "id": 56,
+                "title": "Finish front-end",
+                "children": []
+              },
+              {
+                "id": 78,
+                "title": "Finish back-end",
+                "children": []
+              }
+            ]
+          },
+          {
+            "id": 910,
+            "title": "Finish Legacy Project",
+            "children": []
+          }
+        ]
+      }
+    ];
 
-      $scope.d3OnClick = function(item){
-        console.log(item.id);
-        $scope.currNode = item;
-      };
+    $scope.d3OnClick = function(item){
+      console.log(item.id);
+      $scope.currNode = item;
+    };
 
-      $scope.addTask = function () {
-        $scope.d3Data[0].children.push({"title": $scope.newTask.title});
-        // console.log($scope.d3Data)
-      };
-    });
+    $scope.addTask = function () {
+      $scope.d3Data[0].children[0].children.push({"title": $scope.newTask.title});
+      // console.log($scope.d3Data)
+    };
+  });
 
-}());
+
