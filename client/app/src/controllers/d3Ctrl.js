8e837cd89bb127d1d2da0b3ff59bb3579257f7fb
anicknam <a.nicknam@gmail.com>
2016-10-25 17:02:49 -0700

    Create input box to add new task

--- client/app/src/controllers/d3Ctrl.js
+++ client/app/src/controllers/d3Ctrl.js
@@ -7,20 +7,25 @@
 
       $scope.d3Data = [
         {
+          "id": 12,
           "title": "Graduate from HR!!!",
           "children": [
             {
+              "id": 34,
               "title": "Finish Greenfield Project",
               "children": [
                 {
+                  "id": 56,
                   "title": "Finish front-end",
                 },
                 {
+                  "id": 78,
                   "title": "Finish back-end",
                 }
               ]
             },
             {
+              "id": 910,
               "title": "Finish Legacy Project",
             }
           ]
@@ -28,7 +33,13 @@
       ];
 
       $scope.d3OnClick = function(item){
-        console.log(item.title);
+        console.log(item.id);
+        $scope.currNode = item;
+      };
+
+      $scope.addTask = function () {
+        $scope.d3Data[0].children.push({"title": $scope.newTask.title});
+        console.log($scope.d3Data)
       };
     });
 
