22f1afe2218ffb7becaecb09e9e379e3fb6b163e
anicknam <a.nicknam@gmail.com>
2016-10-26 00:34:37 -0700

    Add checkbox for completion status. Add current node title to current node box

--- client/app/src/controllers/d3Ctrl.js
+++ client/app/src/controllers/d3Ctrl.js
@@ -1,25 +1,28 @@
 
 angular.module('myApp.controllers')
   .controller('d3Ctrl', function($scope){
-    $scope.title = "GoalTree";
 
     $scope.d3Data = [
       {
         "id": 12,
         "title": "Graduate from HR!!!",
+        "completed": false,
         "children": [
           {
             "id": 34,
             "title": "Finish Greenfield Project",
+            "completed": false,
             "children": [
               {
                 "id": 56,
                 "title": "Finish front-end",
+                "completed": false,
                 "children": []
               },
               {
                 "id": 78,
                 "title": "Finish back-end",
+                "completed": false,
                 "children": []
               }
             ]
@@ -27,15 +30,18 @@ angular.module('myApp.controllers')
           {
             "id": 910,
             "title": "Finish Legacy Project",
+            "completed": false,
             "children": []
           }
         ]
       }
     ];
 
+    $scope.currNode = $scope.d3Data[0];
+    
     $scope.d3OnClick = function(item){
-      console.log(item.id);
       $scope.currNode = item;
+      $scope.$apply();
     };
 
     $scope.addTask = function () {
