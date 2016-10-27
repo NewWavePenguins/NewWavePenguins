af52c0a85a4c4541945679d4ad509a8db6498673
anicknam <a.nicknam@gmail.com>
2016-10-26 17:21:52 -0700

    Use ui-router to implement nested views

--- client/app/src/controllers/d3Ctrl.js
+++ client/app/src/controllers/d3Ctrl.js
@@ -1,43 +1,44 @@
 
 angular.module('myApp.controllers')
-  .controller('d3Ctrl', function($scope, Tasks){
+  .controller('d3Ctrl', function($scope, $stateParams, Tasks){
+    for (var i=0; i < $scope.goals.length; i++) {
+      if ($scope.goals[i].id === Number($stateParams.id)) {
+        $scope.d3Data = $scope.goals[i]; 
+      }
+    }
 
-    // Tasks.getTasksTree().then(function(tasksTree) {
-    //   $scope.d3Data = tasksTree;
-    // });
-
-    $scope.d3Data = {
-        "id": 12,
-        "title": "Graduate from HR!!!",
-        "completed": false,
-        "children": [
-          {
-            "id": 34,
-            "title": "Finish Greenfield Project",
-            "completed": false,
-            "children": [
-              {
-                "id": 56,
-                "title": "Finish front-end",
-                "completed": false,
-                "children": []
-              },
-              {
-                "id": 78,
-                "title": "Finish back-end",
-                "completed": false,
-                "children": []
-              }
-            ]
-          },
-          {
-            "id": 910,
-            "title": "Finish Legacy Project",
-            "completed": false,
-            "children": []
-          }
-        ]
-      };
+    // $scope.d3Data = {
+    //     "id": 12,
+    //     "title": "Graduate from HR!!!",
+    //     "completed": false,
+    //     "children": [
+    //       {
+    //         "id": 34,
+    //         "title": "Finish Greenfield Project",
+    //         "completed": false,
+    //         "children": [
+    //           {
+    //             "id": 56,
+    //             "title": "Finish front-end",
+    //             "completed": false,
+    //             "children": []
+    //           },
+    //           {
+    //             "id": 78,
+    //             "title": "Finish back-end",
+    //             "completed": false,
+    //             "children": []
+    //           }
+    //         ]
+    //       },
+    //       {
+    //         "id": 910,
+    //         "title": "Finish Legacy Project",
+    //         "completed": false,
+    //         "children": []
+    //       }
+    //     ]
+    //   };
 
     $scope.currNode = $scope.d3Data;
 
@@ -49,6 +50,10 @@ angular.module('myApp.controllers')
     $scope.addTask = function () {
       $scope.d3Data.children[0].children.push({"title": $scope.newTask.title});
       // console.log($scope.d3Data)
+      Tasks.postChange().then(function(){
+        Tasks.getGoalsArray();
+      })
+
     };
   });
 
