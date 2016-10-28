1340999555161a0de9069345ece5b04ccd1774f0
chichone <cakyle@gmail.com>
2016-10-28 13:38:04 -0700

    Implement function to populate JSON object of tree

--- server/tree-generator.js
+++ server/tree-generator.js
@@ -14,63 +14,107 @@ var mp = require('mongodb-promise');
 // });
 
 exports.generateTree = function(req, res){
-//^^^^^^^^^^^^^^^^^
 
   var goalId = req.params.goalId;
+  var currGoal;
+  var tasksArray;
+//find goal set goal to variable
 
-//$$$$$$$$$$$$$$$$
-  var getTasksOrGoals = function (input) {
+  Goal.findOne({_id: goalId}, function(err, goal) {
+    if (err) throw err;
+    currGoal = {
+      _id: goal._id.toString(),
+      title: goal.title,
+      userId: goal.userId,
+      tasks: goal.tasks,
+      completed: goal.completed,
+      goalId: goal.goalId
+    };
 
-    function findSingleTaskOrGoal(id){
-      var result = {};
-      function findSingleTask(id) {
-        Task.findOne({_id: id}, function(err, task){
-          if (err) throw err;
-          result = task;
-        }).then(function(){cb();}, function(){console.log('err1');});
-      }
 
-      var failed = false;
-      var myGoal = Goal.findOne({_id: id}, function(err, goal){
-        if (err || goal === null || goal['title'] === undefined) { failed = true; }
+    Task.find({goalId: goalId}, function(err, tasksArr) {
+
+      tasksArray = tasksArr.map(function(task){
+        return {
+          _id: task._id.toString(),
+          title: task.title,
+          tasks: task.tasks,
+          completed: task.completed,
+          goalId: task.goalId
+        };
       });
 
-      if (!failed) {
-        myGoal.then(function (goal) {
-          result = goal;
-          cb();
-        })
-      } else {
-        findSingleTask(id);
+      tasksArray.push(currGoal);
+      console.log('TASKSATTAY', tasksArray);
+
+
+      var idToObj = function(input) {
+        if (Array.isArray(input)) {
+          var outArr = input.map(function(val) {
+            for (var i = 0; i < tasksArray.length; i++) {
+              if (tasksArray[i]['_id'].toString() === val) {
+                // console.log('IM INSIDE IF')
+                return tasksArray[i];
+              }
+            }
+          })
+          // console.log('outArr', outArr)
+          return outArr;
+        } else {
+          console.log('in else');
+          for (var j = 0; j < tasksArray.length; j++) {
+            // console.log('tasksArr_id', tasksArray[j]['_id'], 'input', input)
+            if (tasksArray[j]['_id'].toString() === input) {
+              return tasksArray[j];
+            }
+          }
+        }
       }
-      function cb() { return result; }
-    }
 
-
-    // var query = Band.findOne({name: "Guns N' Roses"});
-    // assert.ok(!(query instanceof require('mpromise')));
-
-    // // A query is not a fully-fledged promise, but it does have a `.then()`.
-    // query.then(function (doc) {
-    //   // use doc
-    // });
-
-    if (Array.isArray(input)) {
-      var myMap = [];
-      var mapify = function(input){
-        myMap = input.map(function(id) { findSingleTaskOrGoal(id); });
+      var transformChildren = function(obj) {
+        var objOut = {};
+        for (var i in obj) {
+          objOut[i] = obj[i];
+        }
+        var goodArr = idToObj(obj['tasks']);
+        objOut['tasks'] = goodArr;
+        return objOut;
       }
-      mapify(input).then(function() {return myMap})
 
-    } else {
+      var fullObj = function(rootId) {
+
+        var myObj = idToObj(rootId);
+        // console.log('myObj', myObj)
 
 
-      return findSingleTaskOrGoal(input);
+        var recurse = function(obj) {
+          var outObj = {};
+          for (var i in obj) outObj[i] = obj[i];
+            // console.log('obj', obj);
+          if (obj['tasks'].length === 0) return outObj;
+          outObj = transformChildren(obj);
+          outObj['tasks'] = outObj['tasks'].map(function(child) {
+            // console.log('outObj', outObj)
+            return recurse(child);
+          });
+          return outObj;
+        }
 
-    }
+        return recurse(myObj);
+      }
 
-  };
-//$$$$$$$$$$$$$$$$$$$
+      // console.log(fullObj(goalId));
+      res.status(200).send(fullObj(goalId));
+    });
+  });
+
+};
+
+    //find all tasks with goal id
+
+
+
+
 
 // var greetingPromise = sayHello();
 // greetingPromise.then(function (greeting) {
@@ -78,50 +122,49 @@ exports.generateTree = function(req, res){
 // });
 
 
-  var transformChildren = function(obj) {
-    var objOut = {};
-    for (var i in obj) objOut[i] = obj[i];
-    var goodArr = getTasksOrGoals(obj['tasks']);
-    objOut['tasks'] = goodArr;
-    return objOut;
-  }
-
-  var fullObj = function(rootId) {
-    var myObj = getTasksOrGoals(rootId);
-
-    var recurse = function(obj) {
-
-      var outObj = {};
-      for (var i in obj) outObj[i] = obj[i];
-      if (obj['tasks'].length === 0) return obj;
-      outObj = transformChildren(obj);
-      outObj['children'] = outObj['children'].map(function(child) {
-        return recurse(child);
-      });
-      return outObj;
-
-    }
-  }
-
-  // function foo(){
-  //     return Promise.resolve("Value");
+  // var transformChildren = function(obj) {
+  //   var objOut = {};
+  //   for (var i in obj) objOut[i] = obj[i];
+  //   var goodArr = getTasksOrGoals(obj['tasks']);
+  //   objOut['tasks'] = goodArr;
+  //   return objOut;
   // }
 
-  // foo().then(alert);
-  var finalOut = 'hey';
-  function go(goalId) {
-    finalOut = fullObj(goalId);
-    return Promise.resolve(finalOut);
-  }
+  // var fullObj = function(rootId) {
+  //   var myObj = getTasksOrGoals(rootId);
 
-  go(goalId).then(function() { finalCB(); });
+  //   var recurse = function(obj) {
 
-  function finalCB() { res.status(200).send(finalOut); }
+  //     var outObj = {};
+  //     for (var i in obj) outObj[i] = obj[i];
+  //     if (obj['tasks'].length === 0) return obj;
+  //     outObj = transformChildren(obj);
+  //     outObj['children'] = outObj['children'].map(function(child) {
+  //       return recurse(child);
+  //     });
+  //     return outObj;
+
+  //   }
+  // }
+
+  // // function foo(){
+  // //     return Promise.resolve("Value");
+  // // }
+
+  // // foo().then(alert);
+  // var finalOut = 'hey';
+  // function go(goalId) {
+  //   finalOut = fullObj(goalId);
+  //   return Promise.resolve(finalOut);
+  // }
+
+  // go(goalId).then(function() { finalCB(); });
+
+  // function finalCB() { res.status(200).send(finalOut); }
 
 
 
 //VVVVVVVVVVVVVVVVVV
-};
 
 
 
