c32239ec1b8552df1241529e08d43909f595c096
chichone <cakyle@gmail.com>
2016-10-28 17:40:50 -0700

    Connect real data

--- client/app/src/controllers/home.js
+++ client/app/src/controllers/home.js
@@ -1,70 +1,73 @@
-angular.module('home.controllers', ['ui.router'])
-.controller('homeController', function($scope, $http) {
+angular.module('home.controllers', ['ui.router', 'myApp.services'])
+.controller('homeController', function($scope, $http, Goals) {
+//5813e667ac5ac74563b28bbf
 
-  $scope.goals = [
-   {
-           "id": 12,
-           "title": "Graduate from HR!!!",
-           "completed": false,
-           "children": [
-             {
-               "id": 34,
-               "title": "Finish Greenfield Project",
-               "completed": false,
-               "children": [
-                 {
-                   "id": 56,
-                   "title": "Finish front-end",
-                   "completed": false,
-                   "children": []
-                 },
-                 {
-                   "id": 78,
-                   "title": "Finish back-end",
-                   "completed": false,
-                   "children": []
-                 }
-               ]
-             },
-             {
-               "id": 910,
-               "title": "Finish Legacy Project",
-               "completed": false,
-               "children": []
-             }
-           ]
-         },
-         {
-                 "id": 3300,
-                 "title": "Sth different",
-                 "completed": false,
-                 "children": [
-                   {
-                     "id": 34,
-                     "title": "Finish Greenfield Project",
-                     "completed": false,
-                     "children": [
-                       {
-                         "id": 56,
-                         "title": "Finish front-end",
-                         "completed": false,
-                         "children": []
-                       },
-                       {
-                         "id": 78,
-                         "title": "Finish back-end",
-                         "completed": false,
-                         "children": []
-                       }
-                     ]
-                   },
-                   {
-                     "id": 910,
-                     "title": "Finish Legacy Project",
-                     "completed": false,
-                     "children": []
-                   }
-                 ]
-               }
-  ];
+  $scope.goals = Goals.getGoalsArray();
+  console.log($scope.goals);
+  //  [
+  //  {
+  //          "id": 12,
+  //          "title": "Graduate from HR!!!",
+  //          "completed": false,
+  //          "children": [
+  //            {
+  //              "id": 34,
+  //              "title": "Finish Greenfield Project",
+  //              "completed": false,
+  //              "children": [
+  //                {
+  //                  "id": 56,
+  //                  "title": "Finish front-end",
+  //                  "completed": false,
+  //                  "children": []
+  //                },
+  //                {
+  //                  "id": 78,
+  //                  "title": "Finish back-end",
+  //                  "completed": false,
+  //                  "children": []
+  //                }
+  //              ]
+  //            },
+  //            {
+  //              "id": 910,
+  //              "title": "Finish Legacy Project",
+  //              "completed": false,
+  //              "children": []
+  //            }
+  //          ]
+  //        },
+  //        {
+  //                "id": 3300,
+  //                "title": "Sth different",
+  //                "completed": false,
+  //                "children": [
+  //                  {
+  //                    "id": 34,
+  //                    "title": "Finish Greenfield Project",
+  //                    "completed": false,
+  //                    "children": [
+  //                      {
+  //                        "id": 56,
+  //                        "title": "Finish front-end",
+  //                        "completed": false,
+  //                        "children": []
+  //                      },
+  //                      {
+  //                        "id": 78,
+  //                        "title": "Finish back-end",
+  //                        "completed": false,
+  //                        "children": []
+  //                      }
+  //                    ]
+  //                  },
+  //                  {
+  //                    "id": 910,
+  //                    "title": "Finish Legacy Project",
+  //                    "completed": false,
+  //                    "children": []
+  //                  }
+  //                ]
+  //              }
+  // ];
 });
--- client/app/src/services/server-requests.js
+++ client/app/src/services/server-requests.js
@@ -1,11 +1,12 @@
 angular.module('myApp.services',[])
-.factory('Tasks', function($http) {
+.factory('Goals', function($http) {
 
   var getGoalsArray = function() {
     return $http({
     	method: 'GET',
-    	url: '/getGoalsArray'
+    	url: '/allGoals/5813e667ac5ac74563b28bbf'
     }).then(function(res){
+      console.log(res.data);
     	return res.data;
     })
   };
