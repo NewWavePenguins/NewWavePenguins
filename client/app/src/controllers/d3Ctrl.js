f6b18c88b0ef9e99aa7abd698b1c839523022e2a
anicknam <a.nicknam@gmail.com>
2016-10-29 15:12:43 -0700

    Start looking into rerendering

--- client/app/src/controllers/d3Ctrl.js
+++ client/app/src/controllers/d3Ctrl.js
@@ -1,48 +1,13 @@
 
 angular.module('myApp.controllers')
-  .controller('d3Ctrl', function($scope, $stateParams, $window){
+  .controller('d3Ctrl', function($scope, $stateParams, $http, Goals, $state){
    
     for (var i=0; i < $scope.goals.length; i++) {
       if ($scope.goals[i].id === $stateParams.id) {
-        // console.log('Found the goal');
         $scope.d3Data = $scope.goals[i];
 
       }
     }        
-  
-    
-    // $scope.d3Data = {
-    //     "id": 12,
-    //     "title": "Graduate from HR!!!",
-    //     "completed": false,
-    //     "children": [
-    //       {
-    //         "id": 34,
-    //         "title": "Finish Greenfield Project",
-    //         "completed": false,
-    //         "children": [
-    //           {
-    //             "id": 56,
-    //             "title": "Finish front-end",
-    //             "completed": false,
-    //             "children": []
-    //           },
-    //           {
-    //             "id": 78,
-    //             "title": "Finish back-end",
-    //             "completed": false,
-    //             "children": []
-    //           }
-    //         ]
-    //       },
-    //       {
-    //         "id": 910,
-    //         "title": "Finish Legacy Project",
-    //         "completed": false,
-    //         "children": []
-    //       }
-    //     ]
-    //   };
 
     $scope.currNode = $scope.d3Data;
 
@@ -52,13 +17,29 @@ angular.module('myApp.controllers')
     };
 
     $scope.addTask = function () {
-      $scope.d3Data.children[0].children.push({"title": $scope.newTask.title});
-      // console.log($scope.d3Data)
-      // Tasks.postChange().then(function(){
-      //   Tasks.getGoalsArray();
-      //})
-
-    };
+      // $scope.d3Data.children[0].children.push({"title": $scope.newTask.title});
+      
+      // Add to DB
+      $http({
+        method: 'POST',
+        url: '/addTask',
+        data: {
+            "title" : $scope.newTask.title,
+            "parentId": $scope.currNode.id
+        }
+      }).then(function(){
+        $state.reload();
+        // Add to front end scope.d3Data
+        // resursive function to traverse d3Data and inject the new Task at the right place
+        // var taskToStore = {
+        //    "id": "5814eec179e36cfba0ba72fe",
+        //    "title": $scope.newTask.title,
+        //    "children": [],
+        //    "completed": false, 
+        // };
+      // });
+    });
+  };
 
     $scope.addGoal = function() {
         console.log('clicked')
